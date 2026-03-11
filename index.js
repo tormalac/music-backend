26ef5036a6238e9e32de45758f63f31a51041121
tormalac <tormalac@gmail.com>
2026-03-11 09:10:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ import fs from "fs"; // <-- ÚJ: Fájlrendszer kezeléséhez
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*', // Mindent átenged, még a 'null' origint is
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
@@ -132,6 +136,7 @@ app.post("/duplicate", upload.single("file"), (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
